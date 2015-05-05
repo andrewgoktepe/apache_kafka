b44df79e39ab898c30ecd2dd809d442ba38ec8fb
Andrew Goktepe <andrew.goktepe@savant.com>
2015-05-05 12:32:52 -0700

    hard code version only on my branch

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,8 @@ maintainer_email "mathyourlife@gmail.com"
 license          "Apache-2.0"
 description      "Installs/Configures Apache Kafka broker"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          IO.read(File.join(File.dirname(__FILE__), "VERSION")) rescue "0.0.1"
+#version          IO.read(File.join(File.dirname(__FILE__), "VERSION")) rescue "0.0.1"
+version          "1.2.0"
 
 %w{ ubuntu debian }.each do |os|
   supports os
