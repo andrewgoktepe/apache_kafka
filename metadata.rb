f9b9a677dc78a7796648750045ae80a5c78429b7
Andrew Goktepe <andrew.goktepe@savantsystems.com>
2016-05-11 15:29:45 -0700

    set version to 1.5.6 to match recent merge

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ license          "Apache-2.0"
 description      "Installs/Configures Apache Kafka >= 0.7.0"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 #version          IO.read(File.join(File.dirname(__FILE__), "VERSION")) rescue "0.0.1"
-version          "1.2.0"
+version          "1.5.6"
 
 %w{ ubuntu debian }.each do |os|
   supports os
